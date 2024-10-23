35a6432143c14765f65cdf8bab40fad1ef3ca783
weitao.wang <15537588047@163.com>
2024-10-23 20:38:52 +0800

    mapreduce commit 1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,28 +6,26 @@ import "os"
 import "net/rpc"
 import "net/http"
 
-
 type Coordinator struct {
 	// Your definitions here.
-
+	State      int // 如Map/Reduce, 当前0表示Map
+	MapChan    chan *TaskResponse
+	ReduceChan chan *TaskResponse
+	ReduceNum  int
+	Files      []string
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +39,47 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		State:      0,
+		MapChan:    make(chan *TaskResponse, len(files)),
+		ReduceChan: make(chan *TaskResponse, nReduce),
+		ReduceNum:  nReduce,
+		Files:      files,
+	}
 
 	// Your code here.
-
-
+	c.MakeMapTask(files)
 	c.server()
 	return &c
+}
+
+func (c *Coordinator) MakeMapTask(files []string) {
+	for i, file := range files {
+		task := TaskResponse{
+			TaskType:  0,
+			TaskId:    i,
+			Filename:  file,
+			ReduceNum: c.ReduceNum,
+		}
+		c.MapChan <- &task
+	}
+}
+
+func (c *Coordinator) PullTask(taskReq *TaskRequest, taskResp *TaskResponse) error {
+	*taskResp = *<-c.MapChan
+	return nil
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,113 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
+	//CallExample()
+	// GetTask
+	for i := 0; i < 2; i++ {
+		task := GetTask()
+		DoMapTask(&task, mapf)
+	}
 }
 
-//
+// Get a Task
+func GetTask() TaskResponse {
+	taskReq := TaskRequest{}
+	taskResp := TaskResponse{}
+	ok := call("Coordinator.PullTask", &taskReq, &taskResp)
+	if ok {
+		fmt.Printf("success get task : %v\n", taskResp)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	return taskResp
+}
+
+func DoMapTask(task *TaskResponse, mapf func(string, string) []KeyValue) {
+	/***
+	Map任务主要流程分为三部分：
+	1. 根据RPC调用获取到文件名，调用已经写好的Map函数生成中间文件(intermediate)
+	2. 根据RPC返回的任务参数中的ReduceNum的数值对中间文件(intermediate)进行分组,
+		分组依据根据字母结构体的Key取Hash值随后对ReduceNum取余。
+	3. 将分组后的中间文件保存到临时文件中
+	---------------------个人理解-----------------------------------------
+	生成的临时文件名：mr-X-Y(X是文件id，Y是哈希后对应ReduceNum)
+	对于文件Id就是运行coordinator传入第二个参数files(文件集)中文件的顺序
+	也就是说Map函数的主要功能是将传入的某个文件的词频统计出来(准确的说并没有统计词频，可以阅读wc.go源代码)仅仅将「单词-1」统计出来
+		for _, w := range words {
+		kv := mr.KeyValue{w, "1"}
+		kva = append(kva, kv)
+	随后将某个文件的所有词频根据Key也就是单词，根据单词的哈希值将单词分组，分组个数为ReduceNum
+	*/
+	var intermediate []KeyValue
+	fmt.Printf("worker is map taskId : %v, fileName : %v\n", task.TaskId, task.Filename)
+	file, err := os.Open(task.Filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", task.Filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", task.Filename)
+	}
+	file.Close()
+	intermediate = mapf(task.Filename, string(content))
+	reduceNum := task.ReduceNum
+	hashKV := make([][]KeyValue, reduceNum)
+	for _, value := range intermediate {
+		index := ihash(value.Key) % reduceNum
+		hashKV[index] = append(hashKV[index], value)
+	}
+	// 放入中间文件
+	for i := 0; i < reduceNum; i++ {
+		fileName := "../tempfiles/mr-" + strconv.Itoa(task.TaskId) + "-" + strconv.Itoa(i)
+		tempFile, err := os.Create(fileName)
+		if err != nil {
+			log.Fatalf("create temp file: %v failed.", fileName)
+		}
+		enc := json.NewEncoder(tempFile)
+		for _, kv := range hashKV[i] {
+			err = enc.Encode(&kv)
+			if err != nil {
+				log.Fatalf("encode error: %v", err)
+			}
+		}
+		tempFile.Close()
+	}
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +132,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
